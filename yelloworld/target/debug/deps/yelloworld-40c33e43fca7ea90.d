733db61358d49b852e91d94afbf0819c72ac13c8
parzaech <yash.varshney1103@gmail.com>
2025-01-06 19:43:01 +0530

    solved the issue

---
+++ yelloworld/target/debug/deps/yelloworld-40c33e43fca7ea90.d
@@ -0,0 +1,5 @@
+/Users/parz/rustprojects/yelloworld/target/debug/deps/yelloworld-40c33e43fca7ea90: src/main.rs
+
+/Users/parz/rustprojects/yelloworld/target/debug/deps/yelloworld-40c33e43fca7ea90.d: src/main.rs
+
+src/main.rs:
---
+++ yelloworld/target/debug/yelloworld.d
@@ -0,0 +1,1 @@
+/Users/parz/rustprojects/yelloworld/target/debug/yelloworld: /Users/parz/rustprojects/yelloworld/src/main.rs
